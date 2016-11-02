cacbc1cee1cbb9d4513b5c2aa05c5dc36a09f01f
Ricardo Seromenho <ricardo@seromenho.com>
2016-11-02 18:32:27 +0000

    GA events

--- js/build.js
+++ js/build.js
@@ -6,6 +6,7 @@ $('.linked[data-panel-item-id]').click(function (event) {
     var itemData = _.find(data.items,{id: $(this).data('panel-item-id')});
 
     if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+        Fliplet.Analytics.trackEvent('link', 'screen', itemData.title);
         Fliplet.Navigate.to(itemData.linkAction);
     }
 });
