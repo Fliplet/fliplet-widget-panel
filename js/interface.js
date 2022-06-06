7ef0a93d7bd88f2b24ceac340a567837490a9261
Serhii Krupnyk <84472843+skrupnyk@users.noreply.github.com>
2022-06-06 08:47:47 +0300

    fixed automatic opening of element after drag and drop (#32)

--- js/interface.js
+++ js/interface.js
@@ -36,6 +36,7 @@ setTimeout(function() {
     placeholder: 'panel panel-default placeholder tile',
     cursor: '-webkit-grabbing; -moz-grabbing;',
     axis: 'y',
+    opacity: 0.7,
     start: function(event, ui) {
       dragging = true;
       var itemId = $(ui.item).data('id');
@@ -52,8 +53,7 @@ setTimeout(function() {
       });
 
       $('.panel-collapse.in').collapse('hide');
-      ui.item.addClass('focus').css('height', ui.helper.find('.panel-heading').outerHeight() + 2);
-      $('.panel').not(ui.item).addClass('faded');
+      ui.item.css('height', ui.helper.find('.panel-heading').outerHeight() + 2);
     },
     stop: function(event, ui) {
       var itemId = $(ui.item).data('id');
@@ -65,15 +65,12 @@ setTimeout(function() {
       $('[data-id="' + itemId + '"] .add-link').html('');
       initLinkProvider(movedItem);
 
-      ui.item.removeClass('focus');
-
       var sortedIds = $(".panel-group").sortable("toArray", {
         attribute: 'data-id'
       });
       data.items = _.sortBy(data.items, function(item) {
         return sortedIds.indexOf(item.id);
       });
-      $('.panel').not(ui.item).removeClass('faded');
 
       dragging = false;
 
@@ -171,6 +168,11 @@ $(".tab-content")
     checkPanelLength();
 
     save();
+  })
+  .on('click', function(event) {
+    if (dragging) {
+      event.stopPropagation();
+    }
   })
   .on('show.bs.collapse', '.panel-collapse', function() {
     if (dragging) {
